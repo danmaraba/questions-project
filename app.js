520d6e21cae9b440ff51d71722f1622340eb2951
danmaraba <danielkmaraba@gmail.com>
2024-05-20 17:13:05 +0300

    added an eventlistener

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
-const button=document.querySelector('.btn');
-button.addEventListener('click',function(){
-    console.log('clicked');
+// using selectors inside the element
+const questions=document.querySelectorAll('.question');
+// console.log(questions);
+questions.forEach(function(question){
+// console.log(question);
+const btn=question.querySelector('.btn')
+console.log(btn);
+btn.addEventListener('click',function(){
+    questions.forEach(function(item){
+        if(item !==question){
+            item.classList.remove('show-text');
+        }
+    })
+    question.classList.toggle('show-text')
 })
+})
+// traversing the dom
+// const btns=document.querySelectorAll('.btn');
+// btns.forEach(function(btn){
+//     btn.addEventListener('click',function(e){
+//         const question=e.currentTarget.parentElement;
+//         question.classList.toggle('show-text')
+//     })
+// })
